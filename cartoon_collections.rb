7dbbcaed6ae6859c7a6a2ae3aca380e5e4d4b44f
Nick Corona <coronaisme@gmail.com>
2019-12-05 18:24:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  roll = array.each_with_index do |array, index|
+  puts " #{(index + 1)}. #{array}"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  capt_planet = []
+  array.map do |array|
+  capt_planet << array.capitalize + "!"
+i+= 1 
 end
+capt_planet
+end 
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+   i = 0 
+  if array.any? {|i| i.length > 4}
+    return true 
+  else
+    return false 
+    i+=1 
+end
 end
 
 def find_the_cheese# code an argument here
